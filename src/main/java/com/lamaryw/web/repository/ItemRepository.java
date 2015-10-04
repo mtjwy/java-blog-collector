5f8b29dd5dc3eeea53e1e0a69080a01cdad7a732
Yu Wu <wuyucsu@gmail.com>
2015-10-04 16:44:13 -0500

    add paging and sorting in user-detail page, using Spring Data JPA

--- src/main/java/com/lamaryw/web/repository/ItemRepository.java
+++ src/main/java/com/lamaryw/web/repository/ItemRepository.java
@@ -2,6 +2,7 @@ package com.lamaryw.web.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lamaryw.web.entity.Blog;
@@ -11,5 +12,5 @@ import com.lamaryw.web.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
-	List<Item> findByBlog(Blog blog);
+	List<Item> findByBlog(Blog blog, Pageable pageable);
 }
