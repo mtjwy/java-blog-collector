8762dd659f3e4d9ce4f75013585694880e592129
Yu Wu <wuyucsu@gmail.com>
2015-10-04 13:55:14 -0500

    add Spring MVC and use JSTL (Java Standard Tag Library) in jsp

---
+++ src/main/java/com/lamaryw/web/service/UserService.java
@@ -0,0 +1,20 @@
+package com.lamaryw.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lamaryw.web.entity.User;
+import com.lamaryw.web.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+}
