0b6bb7f28aa00e00ff4210da93bbba7800d9c5f2
Yu Wu <wuyucsu@gmail.com>
2015-10-05 10:59:10 -0500

    use PreAuthorize annotation to prevent user delete other user's blogs

--- src/main/java/com/lamaryw/web/controller/UserController.java
+++ src/main/java/com/lamaryw/web/controller/UserController.java
@@ -76,7 +76,9 @@ public class UserController {
 	
 	@RequestMapping("/blog/remove/{id}")
 	public String removeBlog(@PathVariable int id) {
-		blogService.delete(id);
+		
+		Blog blog = blogService.findOne(id);
+		blogService.delete(blog);
 		
 		return "redirect:/account.html";
 
--- src/main/java/com/lamaryw/web/service/BlogService.java
+++ src/main/java/com/lamaryw/web/service/BlogService.java
@@ -1,6 +1,8 @@
 package com.lamaryw.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.lamaryw.web.entity.Blog;
@@ -22,10 +24,15 @@ public class BlogService {
 		blog.setUser(user);
 		blogRepository.save(blog);
 	}
-
-	public void delete(int id) {
-		blogRepository.delete(id);
+	
+	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+	public void delete(@P("blog") Blog blog) {
+		blogRepository.delete(blog);
 		
+	}
+
+	public Blog findOne(int id) {
+		return blogRepository.findOne(id);
 	}
 
 }
