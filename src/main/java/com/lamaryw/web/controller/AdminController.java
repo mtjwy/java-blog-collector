d04190c3a4359727a387b3c2f86961b17e17fa99
Yu Wu <wuyucsu@gmail.com>
2015-10-05 12:46:52 -0500

    finish some refactoring
    split single controller into multiple controllers.

---
+++ src/main/java/com/lamaryw/web/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.lamaryw.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lamaryw.web.service.UserService;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/users")
+	public String users(Model model) {
+		model.addAttribute("users", userService.findAll());
+		return "users";
+	}
+	
+	@RequestMapping("/users/{id}")
+	public String detail(Model model, @PathVariable int id) {
+		model.addAttribute("user", userService.findOneWithBlogs(id));
+		return "user-detail";
+	}
+	
+	@RequestMapping("users/remove/{id}")
+	public String removeUser(@PathVariable int id) {
+		userService.delete(id);
+		return "redirect:/users.html";
+	}
+}
